9386bd391cd182564af4dd3e572bc6350ac5e36b
sograteful <105254594+sograteful@users.noreply.github.com>
2022-06-30 20:11:37 -0400

    Create quote_loop.js

---
+++ docs/quote_loop.js
@@ -0,0 +1,37 @@
+const text=document.getElementById("quote");
+const author=document.getElementById("author");
+
+const getNewQuote = async () =>
+{
+    //api for quotes
+    var url="https://sograteful.github.io/bank/docs/quotes_array.html";    
+
+    // fetch the data from api
+    const response=await fetch(url);
+    console.log(typeof response);
+    //convert response to json and store it in quotes array
+    const allQuotes = await response.json();
+
+    // Generates a random number between 0 and the length of the quotes array
+    // const indx = Math.floor(Math.random()*allQuotes.length);
+
+    //Store the quote present at the randomly generated index
+    // const quote=allQuotes[indx].text;
+
+    //Store the author of the respective quote
+    // const auth=[indx].author;
+
+
+    //function to dynamically display the quote and the author
+    // text.innerHTML=quote;
+    // author.innerHTML="~ "+auth;
+    
+    allQuotes.forEach(function(quotation) {
+        
+      document.write(quotation.text + "<br />");
+      document.write(quotation.author + "<br /><br />");
+    });
+
+
+}
+getNewQuote();
